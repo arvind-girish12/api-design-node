c8d746e041305ce04a6b7d792d3f791227b9efa6
Arvind Girish <argirish@deloitte.com>
2019-01-07 23:27:21 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -14,13 +14,39 @@ app.use(express.static('client'));
 
 // body parser makes it possible to post JSON to the server
 // we can accss data we post on as req.body
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
 var lions = [];
 var id = 0;
 
+app.get('/lions', (req, res) => {
+    res.send(lions);
+})
+app.get('/lions/:id', (req, res) => {
+    var lion = _.find(lions, { id: req.params.id })
+    res.send(lion || {});
+})
+app.post('/lions', (req, res) => {
+    var lion = req.body;
+    id++
+    lion.id = id + '';
+    lions.push(lion);
+    res.send(lion);
+})
+app.put('/lions/:id', (req, res) => {
+    var lion = req.body;
+    lions[req.params.id] = lion;
+    res.send(lion);
+})
+app.delete('/lions/:id', (req, res) => {
+    var lion = _.find(lions, (lion) => {
+        return lion.id === req.params.id
+    })
+    lions.splice(req.params.id, 1);
+    res.send(lion);
+})
 // TODO: make the REST routes to perform CRUD on lions
 
 app.listen(3000);
